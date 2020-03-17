2d7176d36761c98ac369e41cb282e26223234cc0
Dennis Morello <dennis.morello@contentwise.com>
2020-03-17 13:36:39 +0100

    Added manifest.json and icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,29 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <html lang="en-US" className="antialiased bg-gray-100">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#f56565" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body className="leading-normal text-gray-900">
           <Main />
           <NextScript />
