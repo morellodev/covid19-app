75e07ab4cd97885db7fbe24ec1ca7a337f33b451
Dennis Morello <dennis.morello@contentwise.com>
2020-03-17 11:14:09 +0100

    Added purgecss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"]
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            // Specify the paths to all of the template files in your project
+            content: ["./src/**/*.js"],
+
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        : false
+    ]
+  ]
 };
