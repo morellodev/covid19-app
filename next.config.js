abde640faf42cd8da46dfe7911704e4bc548906c
Dennis Morello <dennis.morello@contentwise.com>
2020-03-18 13:41:03 +0100

    Fixed SW

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 const withOffline = require("next-offline");
 
-module.exports = withOffline();
+const nextConfig = {
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest),
+  workboxOpts: {
+    swDest: "static/service-worker.js"
+  }
+};
+
+module.exports = withOffline(nextConfig);
