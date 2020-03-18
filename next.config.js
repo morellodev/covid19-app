7a8bf4eaf23d8c4f41e928c30ac740222fddb302
Dennis Morello <dennis.morello@contentwise.com>
2020-03-18 13:39:13 +0100

    Cleaned custom workbox config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,3 @@
 const withOffline = require("next-offline");
 
-const nextConfig = {
-  target: "serverless",
-  transformManifest: manifest => ["/"].concat(manifest),
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*|\/api\//,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "httpsAndApiCalls",
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
-};
-
-module.exports = withOffline(nextConfig);
+module.exports = withOffline();
