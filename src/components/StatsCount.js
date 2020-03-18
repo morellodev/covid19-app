5e9cac9673c428c2413eab968e11173bcc81f85f
Dennis Morello <dennis.morello@contentwise.com>
2020-03-18 12:44:52 +0100

    Refactored number formatting logic in StatsCount component

--- src/components/StatsCount.js
+++ src/components/StatsCount.js
@@ -1,10 +1,10 @@
-const formatter = new Intl.NumberFormat();
+const { format: numberFormat } = new Intl.NumberFormat();
 
 const StatsCount = ({ count, label }) => {
   return (
     <div className="flex flex-col px-4 py-3 border-b-2 md:px-8 md:py-6 md:border-b-0 last:border-b-0">
       <p className="text-4xl font-bold text-center text-gray-600 variant-tabular-nums lg:text-6xl">
-        {formatter.format(count)}
+        {numberFormat(count)}
       </p>
       {label && (
         <p className="text-lg font-medium text-center text-gray-500">{label}</p>
