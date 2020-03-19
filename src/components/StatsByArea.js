2606be6e1295d3fb8a422ff40a21727369c86a8f
Dennis Morello <dennis.morello@contentwise.com>
2020-03-19 21:05:02 +0100

    Improved StatsByArea component

--- src/components/StatsByArea.js
+++ src/components/StatsByArea.js
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
+import classNames from "classnames";
 
 const { format: numberFormat } = new Intl.NumberFormat();
 
@@ -13,13 +14,47 @@ const StatsByArea = ({
   canNavigate = true,
   data
 }) => {
-  const renderCellContent = useCallback((countryId, children) => {
-    return canNavigate ? (
-      <Link href="/country/[id]" as={`/country/${countryId}`}>
-        <a className="block px-4 py-2">{children}</a>
-      </Link>
-    ) : (
-      <span className="block px-4 py-2">{children}</span>
+  const { push } = useRouter();
+
+  const navigateToCountry = useCallback(countryId => {
+    push("/country/[id]", `/country/${countryId}`);
+  }, []);
+
+  const renderRow = useCallback(country => {
+    const totalConfirmed = numberFormat(country.totalConfirmed);
+    const totalRecovered = numberFormat(country.totalRecovered);
+    const totalDeaths = numberFormat(country.totalDeaths);
+    const mortality = country.totalConfirmed
+      ? percentFormat(country.totalDeaths / country.totalConfirmed)
+      : "–";
+
+    const onRowClicked = canNavigate
+      ? () => navigateToCountry(country.id)
+      : undefined;
+
+    return (
+      <tr
+        key={country.id}
+        className={classNames(
+          { "cursor-pointer": canNavigate },
+          "transition duration-200 ease-in-out border-b-2 last:border-b-0 hover:bg-gray-100"
+        )}
+        onClick={onRowClicked}
+      >
+        <td className="px-4 py-2">{country.displayName}</td>
+        <td className="px-4 py-2 text-right variant-tabular-nums">
+          {totalConfirmed}
+        </td>
+        <td className="px-4 py-2 text-right variant-tabular-nums">
+          {totalRecovered}
+        </td>
+        <td className="px-4 py-2 text-right variant-tabular-nums">
+          {totalDeaths}
+        </td>
+        <td className="px-4 py-2 text-right variant-tabular-nums">
+          {mortality}
+        </td>
+      </tr>
     );
   }, []);
 
@@ -38,35 +73,7 @@ const StatsByArea = ({
         <tbody>
           {data.areas
             .sort((a, b) => b.totalConfirmed - a.totalConfirmed)
-            .map(country => {
-              const totalConfirmed = numberFormat(country.totalConfirmed);
-              const totalRecovered = numberFormat(country.totalRecovered);
-              const totalDeaths = numberFormat(country.totalDeaths);
-              const mortality = country.totalConfirmed
-                ? percentFormat(country.totalDeaths / country.totalConfirmed)
-                : "–";
-
-              return (
-                <tr
-                  key={country.id}
-                  className="transition duration-200 ease-in-out border-b-2 last:border-b-0 hover:bg-gray-100"
-                >
-                  <td>{renderCellContent(country.id, country.displayName)}</td>
-                  <td className="text-right variant-tabular-nums">
-                    {renderCellContent(country.id, totalConfirmed)}
-                  </td>
-                  <td className="text-right variant-tabular-nums">
-                    {renderCellContent(country.id, totalRecovered)}
-                  </td>
-                  <td className="text-right variant-tabular-nums">
-                    {renderCellContent(country.id, totalDeaths)}
-                  </td>
-                  <td className="text-right variant-tabular-nums">
-                    {renderCellContent(country.id, mortality)}
-                  </td>
-                </tr>
-              );
-            })}
+            .map(country => renderRow(country))}
         </tbody>
       </table>
     </div>
