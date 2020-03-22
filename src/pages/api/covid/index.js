352aa916359f26b40c07e848fafc406bc61d5eaa
Dennis Morello <dennis.morello@contentwise.com>
2020-03-22 12:19:11 +0100

    Removed console.log in api/covid API

--- src/pages/api/covid/index.js
+++ src/pages/api/covid/index.js
@@ -1,7 +1,6 @@
 import { fetchCovidData } from "../../../api/covid";
 
 export default async (req, res) => {
-  console.log(req.headers);
   const covidData = await fetchCovidData();
 
   res.status(200).json(covidData);
