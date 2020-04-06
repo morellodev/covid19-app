aa2c449a1a5bcc56c015197b801792632b43ac86
Dennis Morello <dennis.morello@contentwise.com>
2020-04-06 10:57:08 +0200

    Fixed table header text color to have more contrast with background

--- src/components/CountriesDataTable.js
+++ src/components/CountriesDataTable.js
@@ -63,19 +63,19 @@ const CountriesDataTable = ({ data, isLoading }) => {
           <table className="min-w-full">
             <thead>
               <tr>
-                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
                   Country
                 </th>
-                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
                   Confirmed
                 </th>
-                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
                   Recovered
                 </th>
-                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
                   Deceased
                 </th>
-                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-right text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
                   Mortality
                 </th>
               </tr>
