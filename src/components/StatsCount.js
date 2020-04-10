9e9e7e8258896ebd01572fdb8dc9d0e71661993b
Dennis Morello <dennis.morello@contentwise.com>
2020-04-10 11:35:45 +0200

    Improved Stats layout

--- src/components/StatsCount.js
+++ src/components/StatsCount.js
@@ -2,8 +2,8 @@ const numberFormatter = new Intl.NumberFormat();
 
 const StatsCount = ({ count, label, isLoading }) => {
   return (
-    <div className="flex flex-col px-4 py-3 border-b-2 md:px-8 md:py-6 md:border-b-0 last:border-b-0">
-      <p className="text-4xl font-bold text-center text-gray-700 variant-tabular-nums lg:text-5xl">
+    <div className="flex flex-col px-6 py-4 border-b border-gray-200 md:px-8 md:py-6 md:border-b-0 last:border-b-0">
+      <p className="text-4xl font-bold text-center text-gray-600 variant-tabular-nums lg:text-5xl">
         {isLoading ? "..." : numberFormatter.format(count)}
       </p>
       {label && (
