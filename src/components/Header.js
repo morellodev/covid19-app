39164beaacfbfd922916d49d951b67a43bfb0356
Dennis Morello <dennis.morello@contentwise.com>
2020-04-10 11:50:12 +0200

    Fixed header bottom border on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 flex items-center border-b-2 border-gray-200 bg-gray-50 lg:border-b-0 lg:static">
+    <header className="fixed inset-x-0 top-0 z-50 flex items-center border-b border-gray-200 bg-gray-50 lg:border-b-0 lg:static">
       <div className="relative w-full max-w-screen-xl px-6 mx-auto">
         <div className="flex flex-col justify-center h-16 lg:h-24 lg:border-b-2 lg:border-gray-200">
           <div className="flex items-center -mx-6">
